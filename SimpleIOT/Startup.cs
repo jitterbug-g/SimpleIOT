a24958511ad772ce15aad1d3f09275002ddb2171
greg-sim58 <greg@sim58dev.com>
2017-10-05 16:08:26 +0200

    First Commit

---
+++ SimpleIOT/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SimpleIOT.Startup))]
+namespace SimpleIOT
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
