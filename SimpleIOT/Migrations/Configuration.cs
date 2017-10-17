d47b05ec6ca218a67e953f746e927c2bbc7c115d
greg-sim58 <greg@sim58dev.com>
2017-10-17 18:50:34 +0200

    Added DeviceController, CellController and views and more

--- SimpleIOT/Migrations/Configuration.cs
+++ SimpleIOT/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace SimpleIOT.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(SimpleIOT.Models.ApplicationDbContext context)
